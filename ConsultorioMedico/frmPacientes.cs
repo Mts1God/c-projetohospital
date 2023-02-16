ab6dfb4d79dd9287fc12f616bd55e6910757c5b2
Mts1God <nogueira.mateus20k1@gmail.com>
2023-02-16 17:16:28 -0300

    att

---
+++ ConsultorioMedico/frmPacientes.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+//importando para janela do projeto a abilidade dos correios 
+using Correios.Net;
+
+namespace ConsultorioMedico
+{
+    public partial class frmPacientes : Form
+    {
+        //Criando variáveis para controle do menu
+        const int MF_BYCOMMAND = 0X400;
+        [DllImport("user32")]
+        static extern int RemoveMenu(IntPtr hMenu, int nPosition, int wFlags);
+        [DllImport("user32")]
+        static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+        [DllImport("user32")]
+        static extern int GetMenuItemCount(IntPtr hWnd);
+
+
+        public void desabilitarCampos()
+        {
+            txtCodigo.Enabled = false;
+            txtNome.Enabled = false;
+            txtBairro.Enabled = false;
+            txtCidade.Enabled = false;
+            txtEmail.Enabled = false;
+            txtEndereco.Enabled = false;
+            mskCEP.Enabled = false;
+            mskCPF.Enabled = false;
+            mskTelefone.Enabled = false;
+            cbbEstado.Enabled = false;
+            btnCadastrar.Enabled = false;
+            btnAlterar.Enabled = false;
+            btnExcluir.Enabled = false;
+            btnLimpar.Enabled = false;
+        }
+
+        public void LimparCampos()
+        {
+            txtNome.Text = "";
+            txtBairro.Text = "";
+            txtCidade.Text = "";
+            txtEmail.Text = "";
+            txtEndereco.Text = "";
+            mskCEP.Text = "";
+            mskCPF.Text = "";
+            mskTelefone.Text = "";
+            cbbEstado.Text = "";
+        }
+
+        public void habilitarCampos()
+        {            
+            txtNome.Enabled = true;
+            txtBairro.Enabled = true;
+            txtCidade.Enabled = true;
+            txtEmail.Enabled = true;
+            txtEndereco.Enabled = true;
+            mskCEP.Enabled = true;
+            mskCPF.Enabled = true;
+            mskTelefone.Enabled = true;
+            cbbEstado.Enabled = true;
+            btnCadastrar.Enabled = true;
+            btnLimpar.Enabled = true;
+            txtNome.Focus();
+        }
+
+        //Metodo para carregar a ComboBox estado
+        public void carregarComboBox()
+        {
+            cbbEstado.Items.Add("SP");
+            cbbEstado.Items.Add("RJ");
+            cbbEstado.Items.Add("MG");
+            cbbEstado.Items.Add("BH");
+            cbbEstado.Items.Add("RN");
+            cbbEstado.Items.Add("");
+        } 
+
+        //Construtor da classe 
+        public frmPacientes()
+        {
+            InitializeComponent();
+            desabilitarCampos();
+            carregarComboBox();
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            frmMenuPrincipal abrir = new frmMenuPrincipal();
+            abrir.Show();
+            this.Hide();
+        }
+
+        private void frmPacientes_Load(object sender, EventArgs e)
+        {
+            IntPtr hMenu = GetSystemMenu(this.Handle, false);
+            int MenuCount = GetMenuItemCount(hMenu) - 1;
+            RemoveMenu(hMenu, MenuCount, MF_BYCOMMAND);
+
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            habilitarCampos();
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            frmPesquisar abrir = new frmPesquisar();
+            abrir.ShowDialog();
+
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            //Executando o método verificarCampo
+            verificarCampo();
+
+        }
+
+        //Criando o método para verificar campo vazio
+        public void verificarCampo()
+        {
+            //if (txtNome.Text == "")
+            //{
+            //    MessageBox.Show("Favor inserir valores");
+            //}
+            //else if (txtEmail.Text == "")
+            //{
+            //    MessageBox.Show("Favor inserir valores");
+            //}
+
+            if (txtNome.Text.Equals("") || txtEmail.Text.Equals("") 
+                || mskTelefone.Text.Equals("(  )      -") 
+                || mskCPF.Text.Equals("   .   .   -") 
+                || txtEndereco.Text.Equals("") 
+                || mskCEP.Text.Equals("     -") 
+                || txtBairro.Text.Equals("") 
+                || txtCidade.Text.Equals("") 
+                || cbbEstado.Text.Equals(""))
+            {
+                MessageBox.Show("Favor inserir valores",
+                    "Mensagem do Sistema",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information,
+                    MessageBoxDefaultButton.Button1);
+                txtNome.Focus();
+
+            }
+            else
+            {
+                MessageBox.Show("Favor inserir valores",
+                    "Mensagem do Sistema",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information,
+                    MessageBoxDefaultButton.Button1);
+                desabilitarCampos();
+                LimparCampos();
+
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+            txtNome.Focus();
+        }
+
+        private void btnCarregaEndereco_Click(object sender, EventArgs e)
+        {
+            Address add;
+
+            add = SearchZip.GetAddress(mskCEP.Text, 1000);
+
+            txtEndereco.Text = add.Street;
+            txtBairro.Text = add.District;
+            txtCidade.Text = add.City;
+            cbbEstado.Text = add.City;
+        }   
+    }
+}
